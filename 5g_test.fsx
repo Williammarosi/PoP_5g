f9cff36b32c2a0706e234b064c2573c3ae4f4243
Williammarosi <92114851+Williammarosi@users.noreply.github.com>
2021-10-07 21:13:46 +0200

    Update 5g_test.fsx

--- 5g_test.fsx
+++ 5g_test.fsx
@@ -1,5 +1,7 @@
 open System
-let llst = [[1; 2; 3]; [4; 5; 6;]]
+let llst = [[1; 2; 3]; [4; 5; 6]]
+let lst2 = [[1; 2; 3; 4]; [5; 6; 7; 8]]
+let lst3 = [[1; 2; 3]; [4; 5; 6]; [7; 8; 9]]
 
 let isTable (lst : int list list) =
     let mutable a = 0
@@ -35,9 +37,24 @@ printfn "%A" (dropFirstColumn llst)
 let transposeLstLst (lst: int list list) =
     let mutable a = lst
     let mutable b = []
-    while (a.IsEmpty = false) do
+    while firstColumn a <> [] do
         b <- b @ [firstColumn a]
         a <- dropFirstColumn a
     b
 
 printfn "%A" (transposeLstLst llst)
+printfn "%A" (transposeLstLst lst2)
+printfn "%A" (transposeLstLst lst3)
+
+
+let arr = Array2D.create 2 3 0 in
+    for i = 0 to (Array2D.length1 arr) - 1 do
+        for j = 0 to (Array2D.length2 arr) - 1 do
+            arr.[i,j] <- j + 1 + (i*2 + i*1)
+printfn "%A" arr
+
+
+let transposeArray arra =
+    Array2D.init (Array2D.length2 arra) (Array2D.length1 arra) (fun i j -> arra.[j,i])
+
+printfn "%A" (transposeArray arr)
