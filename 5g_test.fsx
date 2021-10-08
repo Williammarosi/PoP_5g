afaba768a3d69eb51be5a8d9de81f85d63363e72
Williammarosi <92114851+Williammarosi@users.noreply.github.com>
2021-10-08 14:13:29 +0200

    Update 5g_test.fsx

--- 5g_test.fsx
+++ 5g_test.fsx
@@ -1,30 +1,30 @@
 open System
-let llst = [[1; 2; 3]; [4; 5; 6]]
-let lst2 = [[1; 2; 3; 4]; [5; 6; 7; 8]]
-let lst3 = [[1; 2; 3]; [4; 5; 6]; [7; 8; 9]]
+let lst1 = [[1; 2; 3]; [4; 5; 6]]
 
 let isTable (lst : int list list) =
-    let mutable a = 0
-    for elm in lst do
-        a <- a + elm.Length
-    printfn "%A" ((lst.Item 0).Length = a / lst.Length)
-
-isTable llst
+    if List.forall (fun (x : int list) -> x.IsEmpty = true) lst then
+        false
+    else
+        if List.forall (fun (x : int list) -> x.Length = (lst.Item 0).Length) lst then 
+            true
+        else false
+    
+printfn "%A\n" (isTable lst1)
 
 let firstColumn (lst: int list list) =
     let mutable listeny = []
-    if List.contains [] lst then
+    if isTable lst = false then
         listeny <- listeny
     else 
         for elm in lst do
             listeny <- listeny @ [elm.Head]
     listeny
 
-printfn "firstColumn\n%A\n" (firstColumn llst)
+printfn "firstColumn\n%A\n" (firstColumn lst1)
 
 let dropFirstColumn (lst: int list list) =
     let mutable listeny = []
-    if List.contains [] lst then
+    if isTable lst = false then
         listeny <- listeny
     else 
         for elm in lst do
@@ -32,32 +32,17 @@ let dropFirstColumn (lst: int list list) =
     listeny
 
 
-printfn "dropFirstColumn:\n%A\n" (dropFirstColumn llst)
+printfn "dropFirstColumn:\n%A\n" (dropFirstColumn lst1)
 
 let transposeLstLst (lst: int list list) =
     let mutable a = lst
     let mutable b = []
-    while firstColumn a <> [] do
+    while isTable a = true do
         b <- b @ [firstColumn a]
         a <- dropFirstColumn a
     b
 
-printfn "list1:\n%A\n" (transposeLstLst llst)
-printfn "list2:\n%A\n" (transposeLstLst lst2)
-printfn "list3:\n%A\n" (transposeLstLst lst3)
+printfn "transposeList:\n%A\n" (transposeLstLst lst1)
 
-
-let arr = Array2D.create 2 3 0 in
-    for i = 0 to (Array2D.length1 arr) - 1 do
-        for j = 0 to (Array2D.length2 arr) - 1 do
-            arr.[i,j] <- j + 1 + (i*2 + i*1)
-
-let arr2 = Array2D.init 2 3 (fun i j -> j + 1 + (i*2 + i*1))
-
-printfn "Original Array:\n%A\n\n%A\n" arr arr2
-
-
-let transposeArray arra =
-    Array2D.init (Array2D.length2 arra) (Array2D.length1 arra) (fun i j -> arra.[j,i])
-
-printfn "Transposed Array:\n%A\n\n%A\n" (transposeArray arr) (transposeArray arr2)
+let lst2 = [[1; 2; 3; 4]; [5; 6; 7; 8]]
+let lst3 = [[1; 2; 3]; [4; 5; 6]; [7; 8; 9]]
